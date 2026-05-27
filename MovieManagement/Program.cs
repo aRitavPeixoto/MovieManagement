84a1bccb69f5917bc67cfc55066116141d2f5b5f
RitaP <ana.peixoto.264115325@msft.cesae.pt>
2026-05-27 14:45:36 +0100

    Adicionar arquivos de projeto.

---
+++ MovieManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
